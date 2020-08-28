66f5664c4d302f17bbb06b37479d2b170eec6568
Kathirmani Sukumar <skathirmani@users.noreply.github.com>
2020-08-28 13:12:56 +0530

    Add files via upload

--- auto_eda.py
+++ auto_eda.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import scipy.stats as st
 
-def get_correlated_metrics(df, thresh=0.6):
+def get_correlated_metrics_long(df, thresh=0.6):
     corr_matrix = df.corr()
     corr_matrix = pd.DataFrame(np.tril(corr_matrix, -1),
                 columns=corr_matrix.columns,
@@ -19,3 +19,17 @@ def get_correlated_metrics(df, thresh=0.6):
                 res.extend(temp.values.tolist())
     res = pd.DataFrame(res, columns=['col1', 'corr', 'col2'])
     return res
+
+def get_correlated_metrics(df, thresh=0.6):
+    corr_matrix = df.corr()
+    thresh=0.6
+    df_corr = pd.DataFrame(np.tril(corr_matrix, -1),
+                          columns=corr_matrix.columns,
+                          index=corr_matrix.index)
+    df_corr_melted = df_corr.reset_index().melt(id_vars='index')
+    df_corr_melted = df_corr_melted[df_corr_melted['value']>= thresh]
+    df_corr_melted = df_corr_melted.rename(columns={'index': 'col1',
+                                                    'variable': 'col2',
+                                                    'value': 'corr'})
+    df_corr_melted = df_corr_melted.reset_index(drop=True)
+    return df_corr_melted
